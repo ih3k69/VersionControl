d8c0af95446cb17e895e8c318857ab5b0919b739
ih3k69 <talos.viktoriaec@gmail.com>
2022-10-06 11:58:13 +0200

    4/4 Adatok betöltése

--- 4HET_ih3k69/4HET_ih3k69/Form1.cs
+++ 4HET_ih3k69/4HET_ih3k69/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,21 @@ namespace _4HET_ih3k69
 {
     public partial class Form1 : Form
     {
+        List<Flat> flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        void LoadData()
+        {
+            flats = context.Flats.ToList();
         }
     }
 }
