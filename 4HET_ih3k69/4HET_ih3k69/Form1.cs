a52834e4b48a80d09ba732cf63ae14849416cb36
ih3k69 <talos.viktoriaec@gmail.com>
2022-10-06 12:24:40 +0200

    4/5-6  Excel eszközök hivatkozása, excel munkafüzet létrehozása

--- 4HET_ih3k69/4HET_ih3k69/Form1.cs
+++ 4HET_ih3k69/4HET_ih3k69/Form1.cs
@@ -8,6 +8,9 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace _4HET_ih3k69
 {
@@ -15,10 +18,17 @@ namespace _4HET_ih3k69
     {
         List<Flat> flats;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+           
+
+           
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -29,5 +39,11 @@ namespace _4HET_ih3k69
         {
             flats = context.Flats.ToList();
         }
+        void CreateExcel()
+        {
+            //CreateTable();
+
+        }
+
     }
 }
