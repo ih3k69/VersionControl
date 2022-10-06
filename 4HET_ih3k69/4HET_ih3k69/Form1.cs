460b39f42639a396e1989fa05b2cc04060623e1f
ih3k69 <talos.viktoriaec@gmail.com>
2022-10-06 15:08:33 +0200

    4/7 Adatok exportálása excelbe

--- 4HET_ih3k69/4HET_ih3k69/Form1.cs
+++ 4HET_ih3k69/4HET_ih3k69/Form1.cs
@@ -11,6 +11,8 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 using System.Data.Entity.Migrations.Model;
+using System.Drawing.Text;
+using System.IO;
 
 namespace _4HET_ih3k69
 {
@@ -25,6 +27,7 @@ namespace _4HET_ih3k69
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
            
 
            
@@ -41,7 +44,108 @@ namespace _4HET_ih3k69
         }
         void CreateExcel()
         {
-            //CreateTable();
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                 CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
+        }
+        void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+          
+               
+            for (int i = 1; i < headers.Length + 1; i++)
+            {
+               xlSheet.Cells[1, i] = headers[i - 1];
+            }
+            object[,] values = new object[flats.Count, headers.Length];
+            int counter = 0;
+            foreach(var item in flats)
+            {
+                values[counter, 0] = item.Code;
+                values[counter, 1] = item.Vendor;
+                values[counter, 2] = item.Side;
+                values[counter, 3] = item.District;
+                if (item.Elevator==true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+               
+
+                values[counter, 5] = item.NumberOfRooms;
+                values[counter, 6] = item.FloorArea;
+                values[counter, 7] = item.Price;
+                
+                values[counter, 8] = "";
+                
+               counter++;
+            }
+            xlSheet.get_Range(
+              GetCell(2, 1),
+              GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+          
+              
+
+
+
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
 
         }
 
