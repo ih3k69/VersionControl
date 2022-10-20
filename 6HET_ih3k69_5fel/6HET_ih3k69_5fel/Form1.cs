41d703cc7c6c430779a1b178977f5bc0f332f49e
ih3k69 <talos.viktoriaec@gmail.com>
2022-10-20 22:18:55 +0200

    5_8Teljes

--- 6HET_ih3k69_5fel/6HET_ih3k69_5fel/Form1.cs
+++ 6HET_ih3k69_5fel/6HET_ih3k69_5fel/Form1.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,13 +18,55 @@ namespace _6HET_ih3k69_5fel
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
         public Form1()
         {
             InitializeComponent();
-            dataGridView1.DataSource = Rates;
+            comboBox.Text = "EUR";
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var rek = new GetCurrenciesRequestBody();
+            var válasz = mnbService.GetCurrencies(rek);
+            var eredmény = válasz.GetCurrenciesResult;
+            var xml2 = new XmlDocument();
+            xml2.LoadXml(eredmény);
+            foreach (XmlElement element in xml2.DocumentElement)
+            {
+                foreach (var item in element.ChildNodes)
+                {
+                    Currencies.Add(((XmlElement)item).InnerText);
+                }
+                
+            }
+            comboBox.DataSource = Currencies;
+            RefreshData();
+
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+            
+            dateTimePicker1.ValueChanged += DateTimePicker1_ValueChanged;
+            dateTimePicker2.ValueChanged += DateTimePicker2_ValueChanged;
+            comboBox.Click += ComboBox_Click;
             xmlfeladat();
-           adatok();
-           
+            adatok();
+            dataGridView1.DataSource = Rates;
+
+        }
+
+        private void DateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+       
+       private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }      
+        private void ComboBox_Click(object sender, EventArgs e)
+        {
+            RefreshData();
         }
         string webszol()
         {
@@ -31,13 +74,16 @@ namespace _6HET_ih3k69_5fel
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox.SelectedItem.ToString(),
+                startDate=dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
+
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+         
             return result;
+
         }
         void xmlfeladat()
         {
@@ -45,16 +91,20 @@ namespace _6HET_ih3k69_5fel
             xml.LoadXml(webszol());
             foreach (XmlElement element in xml.DocumentElement)
             {
-                var rd = new RateData();
-                Rates.Add(rd);
-                
-                    rd.Date = DateTime.Parse(element.GetAttribute("date"));
+                var rate = new RateData();
+                Rates.Add(rate);
+                //dátum
+                    rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                //valuta
                     var childElement = (XmlElement)element.ChildNodes[0];
-                    rd.Currency = childElement.GetAttribute("curr");
+                if (childElement == null)
+                    continue;
+                rate.Currency = childElement.GetAttribute("curr");
+                //érték
                     var unit = decimal.Parse(childElement.GetAttribute("unit"));
                     var value = decimal.Parse(childElement.InnerText);
                     if (unit != 0)
-                    rd.Value = value / unit;
+                    rate.Value = value / unit;
             }
         }
         void adatok()
@@ -62,7 +112,7 @@ namespace _6HET_ih3k69_5fel
             chartRatesData.DataSource = Rates;
             var series = chartRatesData.Series[0];
             series.ChartType=SeriesChartType.Line;
-           // series.XValueMember = "Data";
+            //series.XValueMember = "Data";
             series.YValueMembers = "Value";
             series.BorderWidth = 2;
 
@@ -76,5 +126,9 @@ namespace _6HET_ih3k69_5fel
 
         }
 
+        private void chartRatesData_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
