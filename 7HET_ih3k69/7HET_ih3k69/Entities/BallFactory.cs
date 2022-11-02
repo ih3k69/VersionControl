d9d34ccd543b55e42ed8672d98f81b65a48d76d5
ih3k69 <talos.viktoriaec@gmail.com>
2022-11-02 19:56:59 +0100

    7.3_labdagyár létrehozása

---
+++ 7HET_ih3k69/7HET_ih3k69/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7HET_ih3k69.Entities
+{
+    internal class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
