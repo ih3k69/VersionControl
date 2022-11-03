2596a1ce9ab2111e0ebd3e6646a44ba526bd1dbe
ih3k69 <talos.viktoriaec@gmail.com>
2022-11-03 14:58:26 +0100

    7.6_autó létrehozása

---
+++ 7HET_ih3k69/7HET_ih3k69/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using _7HET_ih3k69.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _7HET_ih3k69.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
