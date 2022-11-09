f293a2f965c4688859f22e70384d45f227298115
ih3k69 <talos.viktoriaec@gmail.com>
2022-11-09 16:05:26 +0100

    8.7_portfólió érték számítás

--- 8HET_ih3k69/8HET_ih3k69/Form1.cs
+++ 8HET_ih3k69/8HET_ih3k69/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,6 +33,17 @@ namespace _8HET_ih3k69
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
             dataGridView2.DataSource = Portfolio;
         }
-        
+        private decimal PortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last =( from x in Ticks
+                               where x.Index == item.Index.Trim() && date <= x.TradingDay
+                               select x).First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
