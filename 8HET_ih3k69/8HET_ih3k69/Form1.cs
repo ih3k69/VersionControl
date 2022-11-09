6f166576b7b72f95c4cb5070376c63270563b26e
ih3k69 <talos.viktoriaec@gmail.com>
2022-11-09 15:34:54 +0100

    8.4_Adatok betöltése

--- 8HET_ih3k69/8HET_ih3k69/Form1.cs
+++ 8HET_ih3k69/8HET_ih3k69/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,16 @@ using System.Windows.Forms;
 
 namespace _8HET_ih3k69
 {
+    
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context=new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
