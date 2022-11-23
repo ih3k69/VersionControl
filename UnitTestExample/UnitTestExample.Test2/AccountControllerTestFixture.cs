31ba9b54cd005bf2b37500cd0b6be14b3f0f0f25
ih3k69 <talos.viktoriaec@gmail.com>
2022-11-23 14:46:36 +0100

    Jelszó validálás előtti jó állapot/tesztelve

--- UnitTestExample/UnitTestExample.Test2/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test2/AccountControllerTestFixture.cs
@@ -6,30 +6,26 @@ using System.Text;
 using System.Threading.Tasks;
 using UnitTestExample.Controllers;
 
-namespace UnitTestExample.Test
+namespace UnitTestExample.Test2
 {
     public class AccountControllerTestFixture
     {
-        
-
-        [
-             Test,
-             TestCase("abcd1234", false),
-             TestCase("irf@uni-corvinus", false),
-             TestCase("irf.uni-corvinus.hu", false),
-             TestCase("irf@uni-corvinus.hu", true)
-        ]
+        [Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+            ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            //Arrange
+            // Arrange
             var accountController = new AccountController();
-            //Act
-            var actualResult = accountController.ValidateEmail(email);
-            //Assert
-            Assert.AreEqual(expectedResult, actualResult);
 
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
-        
-        
     }
 }
---
+++ UnitTestExample/UnitTestExample.Test2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c78eb323-005b-4bf4-9707-1f4d3bc4fdcd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
