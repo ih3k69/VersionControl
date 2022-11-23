c7e660844b33228d0805acf9f06c3dad846f32f6
ih3k69 <talos.viktoriaec@gmail.com>
2022-11-23 11:36:27 +0100

    10/3_Email validálás tesztfüggvény(NUnit lehetséges probléma)

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        // [Test]
+
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+
+        }
+    }
+}
