f2240b288f8d6f81f23d7e2f777b0d830281f911
ih3k69 <talos.viktoriaec@gmail.com>
2022-11-29 10:34:44 +0100

    11.4 Teljes feladat felhasználói felülettel

--- 11HET_ih3k69/11HET_ih3k69/Form1.Designer.cs
+++ 11HET_ih3k69/11HET_ih3k69/Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnbrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(624, 19);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(75, 23);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnbrowse
+            // 
+            this.btnbrowse.Location = new System.Drawing.Point(543, 17);
+            this.btnbrowse.Name = "btnbrowse";
+            this.btnbrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnbrowse.TabIndex = 1;
+            this.btnbrowse.Text = "Browse";
+            this.btnbrowse.UseVisualStyleBackColor = true;
+            this.btnbrowse.Click += new System.EventHandler(this.btnbrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(250, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Népesség fájl";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Záróév";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(59, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2050,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2005,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(92, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2005,
+            0,
+            0,
+            0});
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(686, 379);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnbrowse);
+            this.Controls.Add(this.btnstart);
+            this.Name = "Form1";
+            this.Text = "t";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnbrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- 11HET_ih3k69/11HET_ih3k69/Form1.cs
+++ 11HET_ih3k69/11HET_ih3k69/Form1.cs
@@ -18,38 +18,22 @@ namespace _11HET_ih3k69
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
-        Random rnd=new Random(1234);
+        OpenFileDialog ofd = new OpenFileDialog();
+        List<int> Nők = new List<int>();
+        List<int> Férfiak = new List<int>();
+        Random rnd = new Random(1234);
 
         public Form1()
         {
             InitializeComponent();
-            Population=GetPopulation(@"D:\nép.csv");
-            BirthProbabilities = GetBirth(@"D:\születés.csv");
-            DeathProbabilities = GetDeath(@"D:\halál.csv");
-            for (int i = 2005; i <=2024; i++)
-            {
-                for (int j = 0; j < Population.Count; j++)
-                {
-                    //importante
-                    Simstep(i, Population[j]);
-                   
-                }
-                int nbrOfMales = (from x in Population
-                                  where x.Gender == Gender.Male && x.IsAlive
-                                  select x).Count();
-                int nbrOfFemales = (from x in Population
-                                    where x.Gender == Gender.Female && x.IsAlive
-                                    select x).Count();
-                Console.WriteLine(
-                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", i, nbrOfMales, nbrOfFemales));
-
-            }
-
+            numericUpDown1.Minimum = 2005;
+            numericUpDown1.Maximum = 2025;
+            textBox1.Text = @"D:\nép-teszt.csv";
         }
-        public List<Person> GetPopulation (string csvpath)
+        public List<Person> GetPopulation(string csvpath)
         {
             List<Person> population = new List<Person>();
-            using (StreamReader sr=new StreamReader(csvpath,Encoding.Default))
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
             {
                 while (!sr.EndOfStream)
                 {
@@ -66,7 +50,7 @@ namespace _11HET_ih3k69
         }
         public List<BirthProbability> GetBirth(string csvpath)
         {
-            List<BirthProbability> születés=new List<BirthProbability>();
+            List<BirthProbability> születés = new List<BirthProbability>();
             using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
             {
                 while (!sr.EndOfStream)
@@ -76,7 +60,7 @@ namespace _11HET_ih3k69
                     {
                         Year = int.Parse(sor[0]),
                         NbrOfChildren = int.Parse(sor[1]),
-                        Bprobability=double.Parse(sor[2])
+                        Bprobability = double.Parse(sor[2])
                     });
                 }
             }
@@ -84,7 +68,7 @@ namespace _11HET_ih3k69
         }
         public List<DeathProbability> GetDeath(string csvpath)
         {
-            List<DeathProbability> halálok=new List<DeathProbability>();
+            List<DeathProbability> halálok = new List<DeathProbability>();
             using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
             {
                 while (!sr.EndOfStream)
@@ -92,7 +76,7 @@ namespace _11HET_ih3k69
                     var sor = sr.ReadLine().Split(';');
                     halálok.Add(new DeathProbability()
                     {
-                        Gender = (Gender)Enum.Parse(typeof(Gender),sor[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), sor[0]),
                         Year = int.Parse(sor[1]),
                         Dprobability = double.Parse(sor[2])
                     });
@@ -105,25 +89,75 @@ namespace _11HET_ih3k69
             if (person.IsAlive == false) return;
             byte kor = (byte)(year - person.BirthYear);
             double halálózásiesély = (from x in DeathProbabilities
-                                   where x.Year == kor && x.Gender == person.Gender
-                                   select x.Dprobability).FirstOrDefault();
+                                      where x.Year == kor && x.Gender == person.Gender
+                                      select x.Dprobability).FirstOrDefault();
             var random = rnd.NextDouble();
-            if (random<=halálózásiesély)
+            if (random <= halálózásiesély)
             {
                 person.IsAlive = false;
             }
-            if (person.Gender==Gender.Female&&person.IsAlive)
+            if (person.Gender == Gender.Female && person.IsAlive)
             {
                 var vanegyerek = (from x in BirthProbabilities
-                                  where x.Year == kor 
+                                  where x.Year == kor
                                   select x.Bprobability).FirstOrDefault();
                 var randomgyerek = rnd.NextDouble();
-                if (randomgyerek<=vanegyerek)
+                if (randomgyerek <= vanegyerek)
                 {
-                    Population.Add(new Person { BirthYear = year, Gender = (Gender)(rnd.Next(1,3)), NbrOfChildren = 0 });
+                    Population.Add(new Person { BirthYear = year, Gender = (Gender)(rnd.Next(1, 3)), NbrOfChildren = 0 });
                 }
             }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Simulation();
+            DisplayResult();
+        }
+
+        private void btnbrowse_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+             textBox1.Text=ofd.FileName;
            
+        }
+        void Simulation()
+        {
+            Population = GetPopulation(textBox1.Text);
+            BirthProbabilities = GetBirth(@"D:\születés.csv");
+            DeathProbabilities = GetDeath(@"D:\halál.csv");
+            for (int i = 2005; i <= numericUpDown1.Value; i++)
+            {
+                for (int j = 0; j < Population.Count; j++)
+                {
+                    //importante
+                    Simstep(i, Population[j]);
+
+                }
+                int férfi = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nő = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Nők.Add(nő);
+                Férfiak.Add(férfi);
+            }
+        }
+        void DisplayResult()
+        {
+            for (int i = 2005; i <= numericUpDown1.Value; i++)
+            {
+                string sor = $"Szimulációs év: {i}\n\tFiúk:{Férfiak[i - 2005]}\n\tNők:{Nők[i - 2005]}\n\n";
+                richTextBox1.AppendText(sor);
+            }
         }
     }
 }
