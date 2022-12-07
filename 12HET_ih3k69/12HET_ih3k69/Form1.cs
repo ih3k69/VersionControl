7efdad1c55baface648f5a382dc3d7ab611ed3a7
ih3k69 <talos.viktoriaec@gmail.com>
2022-12-07 17:24:46 +0100

    12.2 játéktér létrehozása

--- 12HET_ih3k69/12HET_ih3k69/Form1.cs
+++ 12HET_ih3k69/12HET_ih3k69/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace _12HET_ih3k69
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+        
         public Form1()
         {
             InitializeComponent();
+            ga = gc.ActivateDisplay(); //játéktér létrehozása
+            this.Controls.Add(ga);
         }
     }
 }
